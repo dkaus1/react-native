5389bd96d3322e394bca8d41f6d6184a4423b1f4
Devesh Kaushal <dkaushal2@sapient.com>
2017-07-27 10:19:39 +0530

    Simple counter app to understand mobX workflow

---
+++ mobxReactNative/src/stores/counter.js
@@ -0,0 +1,13 @@
+import { observable  } from 'mobx';
+
+export default class CounterStore {
+    @observable count = 0;
+
+     increment() {
+        this.count++;
+    }
+
+     decrement() {
+        this.count--;
+    }
+}
